10073f28be083b8ba04c976e1068f3f0269ac55d
Thomas Malt <thomas@malt.no>
2013-03-10 23:08:04 +0100

    minor cleanup

--- web.js
+++ web.js
@@ -101,13 +101,6 @@ app.listen(port, function () {
 app.get("/", ensureAuthenticated, routes.index);
 app.get("/login", routes.login);
 
-// POST /login
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
-//
-//   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
 app.post('/login',
     passport.authenticate('atlassian-crowd', { 
         failureRedirect: '/login', 
