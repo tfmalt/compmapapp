b86099dfd04986b87d234b2a9a12cb9d6066f46d
Thomas Malt <thomas@malt.no>
2013-03-10 19:45:05 +0100

    added routes for authentication flow

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
 
+
+exports.index = function (req, res) {
+    res.send("hello index");
+    console.log("dumping req to test; ", req);
+};
+
+exports.login = function (req, res) {
+    //res.send("hello login");
+    console.log("req flash: ", req.flash());
+    res.render('login', {});
+};
