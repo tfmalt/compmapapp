0c5ab15ac6c9a00162537afe9ea3e4704c030bc8
Thomas Malt <thomas@malt.no>
2013-03-18 01:15:00 +0100

    implementing log in and log out

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
-
+var u    = require('underscore');
+// var jira = require('jira-api');
 
 exports.index = function (req, res) {
-    res.send("hello index");
-    console.log("dumping req to test; ", req);
+    console.log("dumping user to test; ", req.user);
+    var data = {
+        user: req.user
+    };
+
+    res.render('index', data);
 };
 
 exports.login = function (req, res) {
     //res.send("hello login");
-    console.log("req flash: ", req.flash());
-    res.render('login', {});
+    var flash = req.flash();
+    console.log("req flash: ", flash);
+
+    var data = {
+        user: { displayName: "" }
+    };
+    if (u.isArray(flash.error)) {
+        data.errormsg = flash.error[0];
+    }
+
+    console.log("data: ", data);
+    res.render('login', data);
 };
